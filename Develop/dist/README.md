c1213fd8898b20e9e10116623f42cef0a32fa567
Colton Wilson <colton_wilson93@yahoo.com>
2022-12-28 18:17:56 -0800

    Most code done, need to fix badge when there is a space

---
+++ Develop/dist/README.md
@@ -0,0 +1,27 @@
+# Project Title
+![License Badge](https://shields.io/badge/license-GNU AGPLv3-green)
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [License](#license)
+7. [Questions](#questions)
+
+## Description
+Project Description
+## Installation
+
+## Usage
+
+## Contributing
+
+## Tests
+
+## License
+Please see https://www.gnu.org/licenses/agpl-3.0.html to get detailed information for this license
+
+## Questions
+You can find me [HERE](https://github.com/ColtonWilson) on Github
+You can email me at colton_wilson93@yahoo.com if you have any additional questions.
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Describe the steps required to install your project. (I.E. Installation section)",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Examples of your project in use. (I.E. Usage section)",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "Provide guidelines on how other developers can contribute to your project. (I.E. Contribution Guidelines)",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "Provide any tests written for your application and examples on how to run them. (I.E. Test Instructions)",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'ColtonWilson',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Email?",
+        name: 'email',
+        default: 'colton_wilson93@yahoo.com',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email is required.");
+            }
+            return true;
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data, fileName = "./dist/README.md") {
+        fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+     inquirer.prompt(questions)
+    .then (answers => writeToFile(generateMarkdown(answers)))
+}
 
 // Function call to initialize app
 init();
